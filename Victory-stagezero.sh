e3168f0e52420ff526fa1fc6d08e7bb62dbe93dd
Victory-Uzezi <109828999+Victory-Uzezi@users.noreply.github.com>
2022-08-03 14:48:09 +0100

    Create Victory-stagezero.sh

---
+++ Victory-stagezero.sh
@@ -0,0 +1,62 @@
+mkdir test.sh
+
+nano test.sh
+
+	string="Victory"
+	echo $string
+		function bash {
+			local string="Omovrah"
+			echo $string
+			}
+			bash
+mkdir victory
+
+mkdir biocomputing && cd biocomputing
+
+wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+
+Wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+
+Wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+
+mv wildtype.fna victory
+rm wildtype.gbk
+
+grep 'tatatata' wildtype.fna
+grep 'tatatata' wildtype.fna > mutant
+
+clear && history
+
+exit
+
+Sudo apt-get install figlet
+
+cd /etc
+
+nano bash.bashrc 
+
+	Clear
+
+	Figlet -f slant=”Hi Victory”
+
+Mkdir compare && cd compare 
+
+wget https://www.bioinformatics.babraham.ac.uk/training/Introduction%20to%20Unix/unix_intro_data.tar.gz
+
+gunzip unix_intro_data.tar.gz
+
+tar -xvf  unix_intro_data.tar
+
+grep rRNA Mito.dat
+cp Mito.dat compare
+
+nano Mito.dat
+
+Ctrl s+y+x
+
+mv Mito.dat Mitochondrion.txt
+
+wc -l lane8_DD_P4_TTAGGC_L008_R1.fastq.gz 
+
+lp lane8_DD_P9_TGACCA_L008_R1.fastq
+
